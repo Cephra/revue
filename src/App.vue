b17b042f5931548dc762391a25cdee7522a67160
BuildTools <unconfigured@null.spigotmc.org>
2022-06-06 20:24:52 +0200

    feat: added reveal.js and demo presentation, vue works

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="reveal">
+    <div class="slides">
+      <SlideOne/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Reveal from 'reveal.js';
+
+import SlideOne from './components/SlideOne.vue'
 
 export default {
   name: 'App',
+  async mounted() {
+    await Reveal.initialize({
+      autoSlide: 1000,
+      autoSlideStoppable: false,
+      loop: true,
+      progress: false,
+      controls: false,
+      keyboard: false,
+      touch: false,
+      pause: false,
+    });
+  },
+  async beforeUnmounted() {
+    console.log('beforeUnmounted');
+  },
   components: {
-    HelloWorld
+    SlideOne
   }
 }
 </script>
 
 <style>
+@import url('../node_modules/reveal.js/dist/reveal.css');
+@import url('../node_modules/reveal.js/dist/theme/solarized.css');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/SlideOne.vue
@@ -0,0 +1,16 @@
+<template>
+  <section data-auto-animate>
+    <h1>JUST</h1>
+  </section>
+  <section data-auto-animate>
+    <h1>JUST</h1>
+    <h1>DO</h1>
+  </section>
+  <section data-auto-animate data-transition="zoom">
+    <h1>JUST</h1>
+    <h1>DO</h1>
+    <h1>IT.</h1>
+  </section>
+  <section>
+  </section>
+</template>
