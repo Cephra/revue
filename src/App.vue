16e2ccfeb50751b5951c440ee919bf91a62a8f06
Cephra <soulzterx@googlemail.com>
2022-06-17 22:47:01 +0200

    feat: removed obsolete change

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,14 @@ export default {
     await Reveal.initialize({
       autoSlide: 1000,
       autoSlideStoppable: false,
+      
       loop: true,
       progress: false,
+
       controls: false,
       keyboard: false,
       touch: false,
+      
       pause: false,
     });
   },
@@ -39,11 +42,8 @@ export default {
 @import url('../node_modules/reveal.js/dist/theme/solarized.css');
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   height: 100vh;
 }
 [v-cloak] {
