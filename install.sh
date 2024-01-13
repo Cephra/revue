8cc5beee605dfdc8b470f4e6c40ac40be81ab0a5
szt <soulzterx@googlemail.com>
2024-01-13 12:34:22 +0100

    chore(install): improved install script

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 git pull
 yarn install
 yarn build
-sudo rm -rf /srv/http/0x29a.me
-sudo mv dist /srv/http/0x29a.me
+DIR=/srv/http/0x29a.me
+sudo rm -rf $DIR
+sudo mv dist $DIR
+sudo chown -R http:http $DIR
